a254ab56eff36a68c882d0d0fbbd4458910832a0
CR_Avila <carlosrobertoavilah@gmail.com>
2019-01-10 14:57:38 -0600

    Add missing semicolons and delete unnecessary spaces

--- main.js
+++ main.js
@@ -11,14 +11,12 @@ function handleCheck(e){
          
             if(checkbox === this || checkbox === lastChecked){
                 inBetween = !inBetween;
-       
             };
-
             if(inBetween){
                 checkbox.checked = true;
-            }
-        } )
-    }
+            };
+        });
+    };
     lastChecked = this;
 };
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck))
